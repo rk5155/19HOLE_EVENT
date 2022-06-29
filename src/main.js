03d01441acd0afc11e9d69301e1c7a194e5d556b
rk <>
2022-06-29 14:13:17 +0900

    firebase設定完了’

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { initializeApp } from 'firebase/app'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8f8xlna2zteGLTikFz7lx-4DPEUjVATI",
+  authDomain: "hole-golf.firebaseapp.com",
+  projectId: "hole-golf",
+  storageBucket: "hole-golf.appspot.com",
+  messagingSenderId: "73046976270",
+  appId: "1:73046976270:web:9403ce494172943dc0595f",
+  measurementId: "G-878F4B3C9F"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+export default firebaseApp
 
 new Vue({
   render: h => h(App),
