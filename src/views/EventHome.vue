19c6bce530984d343244b024533b7018109b46f3
rk <>
2022-06-29 16:34:20 +0900

    イベント一覧を表示させた

--- src/views/EventHome.vue
+++ src/views/EventHome.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
     <h1>イベント一覧</h1>
+    <div v-for="(event, index) in events" :key="index">
+      {{ event }}
+    </div>
   </div>
 </template>
 
+<script>
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+
+export default {
+  data () {
+    return {
+      events: []
+    }
+  },
+  created () {
+    const db = getFirestore()
+    getDocs(collection(db, "events")).then((result => {
+      result.forEach((doc) => {
+        this.events.push(doc.data())
+      });
+    }))
+  }
+}
+</script>
